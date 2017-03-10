8fa00793bc0274d927cadf208d65a9deba968a7a
linfenliang <linfenliang@126.com>
2017-03-10 19:35:28 +0800

    删除无效代码

--- springboot-data/src/main/java/com/springboot/data/mongodb/controller/VehicleOnlineDataController.java
+++ springboot-data/src/main/java/com/springboot/data/mongodb/controller/VehicleOnlineDataController.java
@@ -5,7 +5,6 @@
 package com.springboot.data.mongodb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
