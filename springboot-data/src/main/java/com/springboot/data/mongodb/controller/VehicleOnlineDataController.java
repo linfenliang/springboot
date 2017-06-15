5bde46b07fadcefda2baf2e0e8ac30d1d58970ca
林芬亮 <linfenliang@126.com>
2017-06-15 17:21:43 +0800

    删除了service

--- springboot-data/src/main/java/com/springboot/data/mongodb/controller/VehicleOnlineDataController.java
+++ springboot-data/src/main/java/com/springboot/data/mongodb/controller/VehicleOnlineDataController.java
@@ -4,12 +4,21 @@
  */
 package com.springboot.data.mongodb.controller;
 
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.springboot.data.mongodb.service.VehicleOnlineDataService;
+import com.springboot.data.mongodb.dao.VehicleOnlineDataRepository;
+import com.springboot.data.mongodb.domain.VehicleOnlineData;
 
 /**
  *
@@ -22,10 +31,18 @@ import com.springboot.data.mongodb.service.VehicleOnlineDataService;
 public class VehicleOnlineDataController {
 
 	@Autowired
-	private VehicleOnlineDataService vehicleOnlineDataService;
+	private VehicleOnlineDataRepository vehicleOnlineDataRepository;
 	@RequestMapping("list")
 	@ResponseBody
-	public void showData(){
-		System.out.println(vehicleOnlineDataService.getCount());
+	public void showData() throws IOException{
+		System.out.println(vehicleOnlineDataRepository.count());
+		Calendar cal = Calendar.getInstance();
+		cal.set(2017, 5, 14, 18, 22, 18);
+		Date gatherTimeBegin = cal.getTime();
+		cal.add(Calendar.SECOND, 30);
+		Date gatherTimeEnd = cal.getTime();
+		Page<VehicleOnlineData> page = vehicleOnlineDataRepository.findByVehicleIdAndGatherTimeRange("98765432109876543210987654321032A", gatherTimeBegin, gatherTimeEnd,new PageRequest(0, 10,new Sort(Direction.DESC,"gatherTime")));
+
+		
 	}
 }
